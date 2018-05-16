ea5d01286899f5dbdd5c51affb64aeb6826ec713
AlexAegis <alexaegis@gmail.com>
2018-05-16 10:37:08 +0200

    exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,8 @@ import javax.swing.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws Exception {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		Window.getMainWindow();
 	}
 
